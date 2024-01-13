4978b9d3b9d7e47b8753984d7dc25cac3e49de21
Yawavi Etsiam AYAH <ayawaetsiam2004@gmail.com>
2024-01-13 04:22:38 +0000

    la page de setting

--- lib/ui/screens/setting_screen.dart
+++ lib/ui/screens/setting_screen.dart
@@ -4,7 +4,7 @@ import 'package:gap/gap.dart';
 import 'package:klassrum/ui/configs/styles.dart';
 
 class SettingScreen extends StatefulWidget {
-  const SettingScreen({super.key});
+  const SettingScreen({Key? key}) : super(key: key);
 
   @override
   State<SettingScreen> createState() => _SettingScreenState();
@@ -13,7 +13,7 @@ class SettingScreen extends StatefulWidget {
 class _SettingScreenState extends State<SettingScreen> {
   bool switchOff = false;
 
-  void onChangeOperation(bool value){
+  void onChangeOperation(bool value) {
     setState(() {
       switchOff = value;
     });
@@ -27,89 +27,87 @@ class _SettingScreenState extends State<SettingScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: Column(children: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              border: const Border(bottom: BorderSide(
-                color: AppColors.primaryColor,
-                width: 1
-              ))
-            ),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  child: CachedNetworkImage(
-                    imageUrl:
-                        "https://i.ibb.co/1nfH7xw/imresizer-1704975409967.jpg",
-                    placeholder: (context, url) =>
-                        const CircularProgressIndicator(),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
-                  radius: 32,
-                ),
-                const Gap(16),
-                const Expanded(
-                  child: Column(
-                    children: [
-                      Text(
-                        "KELE Bernadin",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
-                        textAlign: TextAlign.center,
-                      ),
-                      Text("Licence Professionnelle en Genie Logiciel")
-                    ],
-                  ),
-                ),
-              ],
-            ),
-
-          ),
-          const Gap(16),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                 const Text("Mode d'ecran",
-                   style: TextStyle(
-                     fontSize: 16
-                   ),
-                 ),
-                SwitchListTile(
-                  title: const Text("Sombre"),
-                  value: switchOff,
-                  activeColor: AppColors.primaryColor,
-                  onChanged: onChangeOperation,
-                ),
-              ],
-            ),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 16),
               decoration: BoxDecoration(
-                  border: const Border(bottom: BorderSide(
-                      color: AppColors.primaryColor,
-                      width: 1
-                  ))
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.grey[100],
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 2),
+                      color: Colors.white,
+                    ),
+                    child: CachedNetworkImage(
+                      imageUrl:
+                          "https://i.ibb.co/1nfH7xw/imresizer-1704975409967.jpg",
+                      placeholder: (context, url) =>
+                          const CircularProgressIndicator(),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                    ),
+                  ),
+                  const Gap(12),
+                ],
               ),
             ),
-          Container(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-            child:Row(
-              children: [
-                Icon(Icons.logout),
-                Gap(8),
-                Text("Deconnexion",
-                  style: TextStyle(
-                      fontSize: 16
+            const Gap(8),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.grey[100],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Row(
+                    children: [
+                      Icon(Icons.light_mode),
+                      Gap(8),
+                      Text(
+                        "Mode d'écran",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                  const Gap(8),
+                  SwitchListTile(
+                    title: const Text("Sombre"),
+                    value: switchOff,
+                    activeColor: AppColors.primaryColor,
+                    onChanged: onChangeOperation,
+                  ),
+                ],
+              ),
             ),
-
-          )
-        ]),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 4),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Row(
+                children: [
+                  Icon(Icons.logout),
+                  Gap(8),
+                  Text(
+                    "Déconnexion",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
